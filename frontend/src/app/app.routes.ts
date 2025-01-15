b9ada1b44cb5d1765497df61a3fec3221fc38044
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-01-15 00:15:21 -0300

    feature: adicionado página forget-password.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginSignUp } from './pages/login-sign-up/sign-up.component';
 import { LoginPageComponent } from './shared/component-login/login-component.component';
 import { NgModule } from '@angular/core';
+import { LoginForgetPassword } from './pages/login-forget-password/forget-password.component';
 
 export const routes: Routes = [
     {
         path: "login", component: LoginPageComponent, children: [
             { path: "", redirectTo: "sign-up", pathMatch: "full" },
             { path: "sign-up", component: LoginSignUp },            
+            { path: "forget-password", component: LoginForgetPassword },   
         ]
     }
 ];
---
+++ frontend/src/app/pages/login-forget-password/forget-password.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+    selector: 'login-forget-password',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './forget-password.component.html',
+    styleUrl: './forget-password.component.scss',
+})
+export class LoginForgetPassword {
+    
+}
