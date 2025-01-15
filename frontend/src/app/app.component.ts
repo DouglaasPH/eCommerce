3141b5f5a0fd206c8544001149f848b43c4c3c02
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-01-14 23:43:13 -0300

    feature: adicionado página de login sign up.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { AppRoutingModule } from './app.routes';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginSignUp, LoginPageComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
