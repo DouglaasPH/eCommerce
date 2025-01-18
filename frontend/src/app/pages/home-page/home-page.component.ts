dc4c7e1dee5d7d01d173fb3de17c3220cb101142
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-01-17 21:45:30 -0300

    feature: adicionado componente subscribe to our newslatter.

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -7,11 +7,12 @@ import { Brands } from "./components/brands/brands.component";
 import { WomenCollection } from "../../shared/women-collection/women-collection.component";
 import { FollowUsOnInstagram } from "../../shared/follow-us-on-instagram/follow-us-on-instagram.component";
 import { ThisIsWhatOurCustomersSay } from "../../shared/this-is-what-our-customers-say/this-is-what-our-customers-say.component";
+import { SubscribeToOurNewslatter } from "../../shared/subscribe-to-our-newslatter/subscribe-to-our-newslatter.component";
 
 @Component({
     selector: 'home-page',
     standalone: true,
-    imports: [RouterOutlet, NavBarWithoutLogin, ultimateSale, Brands, DealsOfTheMonth, WomenCollection, FollowUsOnInstagram, ThisIsWhatOurCustomersSay],
+    imports: [RouterOutlet, NavBarWithoutLogin, ultimateSale, Brands, DealsOfTheMonth, WomenCollection, FollowUsOnInstagram, ThisIsWhatOurCustomersSay, SubscribeToOurNewslatter],
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.scss',
 })
---
+++ frontend/src/app/shared/subscribe-to-our-newslatter/subscribe-to-our-newslatter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+    selector: 'subscribe-to-our-newslatter',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './subscribe-to-our-newslatter.component.html',
+    styleUrl: './subscribe-to-our-newslatter.component.scss',
+})
+export class SubscribeToOurNewslatter {
+    
+}
