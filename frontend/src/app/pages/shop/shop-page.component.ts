05d8481f5a825c9e289c015310b3a9b692b3f089
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-01-21 18:13:02 -0300

    feature: finalizado página shop. Futuramente, algumas alterações são necessárias.

--- frontend/src/app/pages/shop/shop-page.component.ts
+++ frontend/src/app/pages/shop/shop-page.component.ts
@@ -2,11 +2,16 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { NavBarWithLogin } from "../../shared/nav-bar/with-login/nav-bar-with-login.component";
 import { FiltersComponent } from "./components/filters/filters-component.component";
+import { WomenCollection } from "../../shared/women-collection/women-collection.component";
+import { FollowUsOnInstagram } from "../../shared/follow-us-on-instagram/follow-us-on-instagram.component";
+import { SubscribeToOurNewslatter } from "../../shared/subscribe-to-our-newslatter/subscribe-to-our-newslatter.component";
+import { FooterBar } from "../../shared/footer-bar/footer-bar.component";
+import { Shop } from "./components/shop/shop.component";
 
 @Component({
     selector: 'shop-page',
     standalone: true,
-    imports: [RouterOutlet, NavBarWithLogin, FiltersComponent],
+    imports: [RouterOutlet, NavBarWithLogin, FiltersComponent, Shop, WomenCollection, FollowUsOnInstagram, SubscribeToOurNewslatter, FooterBar],
     templateUrl: './shop-page.component.html',
     styleUrl: './shop-page.component.scss',
 })
