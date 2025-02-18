1175304a7e56fc4aa6aaf5a75db0cc8d3ab8f593
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-02-18 16:32:50 -0300

    feature: adicionado página para escolher uma forma de pagamento de acordo com as opções disponíveis: PIX ou cartão de crédito.

--- frontend/src/app/pages/payment/payment.component.ts
+++ frontend/src/app/pages/payment/payment.component.ts
@@ -2,14 +2,59 @@ import { Component } from "@angular/core";
 import { navBar } from "../../shared/nav-bar/nav-bar.component";
 import { FooterBar } from "../../shared/footer-bar/footer-bar.component";
 import { CommonModule } from "@angular/common";
+import { OrderSumaryService } from "../../services/orderSumary.service";
+import { Router } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 @Component({
     selector: 'payment',
     standalone: true,
-    imports: [navBar, FooterBar, CommonModule],
+    imports: [navBar, FooterBar, CommonModule, FormsModule],
     templateUrl: './payment.component.html',
     styleUrl: './payment.component.scss',
 })
 export class Payment {
+    constructor(private orderSumaryService: OrderSumaryService, private router: Router) {}
+    shoppingCart: any[] = [];
+    orderSumary = {
+        fullPriceWithoutDiscount: 0,
+        discount: 0,
+        shipping: "undefined",
+        couponApplied: 0,
+        total: 0,
+        estimatedDelivery: "undefined",        
+    }
+
+    creditCardDatas = {
+        card_number: '',
+        namePrintedOrCard: '',
+        validity: '',
+        verificationCade: '',
+        cpfOrCnpj: '',
+        dateOfBirth: '',
+        paymentMethod: '',
+    }
+
+    choosePaymentMethod = '';
+
+    async ngOnInit() {
+        await this.updateProperties();
+    }
+
+    async updateProperties() {
+        const response = await this.orderSumaryService.getAllProperties();
+        this.shoppingCart = response.shoppingCart;
+        this.orderSumary = response.orderSumary;
+    }
+    
+    setChoosePaymentMethod(paymentMethod: string) {
+        this.choosePaymentMethod = paymentMethod;
+    }
+
+    continueToConfirmation() {
+        if (this.choosePaymentMethod !== '') {
+            this.router.navigate(['shopping-cart/address/shipping/payment']);
+        } else return;
+    }
     
 }
