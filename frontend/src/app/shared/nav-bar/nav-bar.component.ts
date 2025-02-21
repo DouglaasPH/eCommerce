60dfdf6a3509bbc3a0a3703d30a1fdfad0688c94
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-02-21 00:23:10 -0300

    feature: removido múltiplos componentes de nav-bar. Tratamento da condição de login em apenas um componente. Novo design à nav-bar.

--- frontend/src/app/shared/nav-bar/nav-bar.component.ts
+++ frontend/src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,27 +1,45 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
-import { NavBarWithLogin } from "./logged-in-user/nav-bar-with-login.component";
-import { NavBarWithoutLogin } from "./user-not-logged-in/nav-bar-without-login.component";
 import { checkLoggined } from "../../requests/loginRequests";
-import { NavigationEnd, Router } from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'nav-bar',
     standalone: true,
-    imports: [CommonModule, NavBarWithLogin, NavBarWithoutLogin],
+    imports: [CommonModule],
     templateUrl: './nav-bar.component.html',
     styleUrl: './nav-bar.component.scss',
 })
 export class navBar implements OnInit {
+    constructor(private router: Router) { }
     isLogginned = false;
+    currentRoute = '';
 
     async ngOnInit() {
+        this.currentRoute = this.router.url;
         await this.displayNavBar();
     }
 
     async displayNavBar() {
         const condition = await checkLoggined();
-        console.log(condition);
         this.isLogginned = condition.isLogginned;
     }
+
+    onButtonSignIn() {
+        this.router.navigate(['/login/sign-in']);
+    }
+
+    onButtonSignUp() {
+        this.router.navigate(['/login/sign-up']);
+    }
+
+    onHome() {
+        this.router.navigate(['/']);
+    }
+
+    onShop() {
+        this.router.navigate(['/shop']);
+    }
+
+
 }
